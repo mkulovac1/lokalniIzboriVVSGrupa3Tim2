b0d44a3ebc31da7ad33aa6ff0fc1af8ae7a7e19c
Merim Kulovac <92273186+mkulovac1@users.noreply.github.com>
2022-11-15 12:50:51 +0100

    Uradjena dorada mandata za kandidate i stranke. Provjereno da li se dodaje glas Kandidatu tj. povecava prilikom glasanja - feedback uradjen - radi funckionalnost.

--- lokalniIzboriVVSGrupa3Tim2/LokalniIzbori.cs
+++ lokalniIzboriVVSGrupa3Tim2/LokalniIzbori.cs
@@ -37,5 +37,100 @@ namespace lokalniIzboriVVSGrupa3Tim2
         public List<Glas> Glasovi { get => glasovi; set => glasovi = value; }
         public List<Stranka> Stranke { get => stranke; set => stranke = value; }
 
+        private bool UkupniIzborniPragKandidata() // da li su izbori legalni
+        {
+            int ukupanBrojMogucihGlasova = Glasaci.Count();
+            int ukupanBrojGlasova = 0;
+            foreach (Glasac g in glasaci)
+            {
+                if (g.GlasaoZaGradonacelnika || g.GlasaoZaVijecnika || g.GlasaoZaNacelnika)
+                    ukupanBrojGlasova++;
+            }
+
+            if (ukupanBrojGlasova >= ukupanBrojMogucihGlasova * 0.2)
+                return true;
+            else
+                return false;
+        }
+
+
+        private int brojGlasovaZaNacelnika()
+        {
+
+            int brojGlasova = 0;
+            foreach (Glasac g in glasaci)
+            {
+                if (g.GlasaoZaNacelnika)
+                    brojGlasova++;
+            }
+            return brojGlasova;
+
+        }
+
+        private int brojGlasovaZaGradonacelnika()
+        {
+
+            int brojGlasova = 0;
+            foreach (Glasac g in glasaci)
+            {
+                if (g.GlasaoZaGradonacelnika)
+                    brojGlasova++;
+            }
+            return brojGlasova;
+        }
+
+        private int brojGlasovaZaVijecnika()
+        {
+            int brojGlasova = 0;
+            foreach (Glasac g in glasaci)
+            {
+                if (g.GlasaoZaVijecnika)
+                    brojGlasova++;
+            }
+            return brojGlasova;
+        }
+
+        public bool DaLiJeKandidatOsvojioMandat(Kandidat k)
+        {
+            // gleda se broj glasaca koji je glasao za odredjenu poziciju i onda se broj glasova kandidata uporedjuje sa tim brojem
+            // dijelimo ovdje zavisni i nezavisni
+            // zavisni mora osvojiti 20 % stranke glasova
+            // nezavisni 2 %
+            if (k.StrankaKandidata == null)
+            {
+                if (k.PozicijaKandidata.Equals(NazivPozicije.nacelnik) && k.BrojGlasova >= 0.02 * brojGlasovaZaNacelnika())
+                    return true;
+                else if (k.PozicijaKandidata.Equals(NazivPozicije.gradonacelnik) && k.BrojGlasova >= 0.02 * brojGlasovaZaGradonacelnika())
+                    return true;
+                else if (k.PozicijaKandidata.Equals(NazivPozicije.vijecnik) && k.BrojGlasova >= 0.02 * brojGlasovaZaVijecnika())
+                    return true;
+            }
+            else // ako je u stranci
+            {
+                // nije bitno koja je pozicija
+                if (k.BrojGlasova >= k.StrankaKandidata.BrojGlasova * 0.2)
+                    return true;
+            }
+
+            return false;
+        }
+
+        public bool DaLiJeStrankaOsvojilaMandat(Stranka s)
+        {
+            // stranka da ima mandat mora osvojiti 2%
+            // stranci se dodijeli glas samo prilikom glasanja za vijecnika/vijecnike!
+            int brojGlasacaVijecnika = 0;
+
+            foreach (Glasac g in glasaci)
+            {
+                if (g.GlasaoZaVijecnika)
+                    brojGlasacaVijecnika++;
+            }
+
+            if (s.BrojGlasova >= 0.02 * brojGlasacaVijecnika)
+                return true;
+            else
+                return false;
+        }
     }
 }
